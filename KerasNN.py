06c82e0c9b784a47aef84bdb868216a9b3f4f318
abhishekpradhan82 <45689920+abhishekpradhan82@users.noreply.github.com>
2019-04-11 12:19:44 +0530

    Create KerasNN.py

---
+++ KerasNN.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+mnist_dataset = tf.keras.datasets.mnist
+
+(x_train,y_train),(x_test,y_test) = mnist_dataset.load_data()
+
+import matplotlib.pyplot as plt
+
+#plt.imshow(x_train[2])
+#plt.show()
+
+mymodel = tf.keras.models.Sequential()
+
+mymodel.add(tf.keras.layers.Flatten())
+mymodel.add(tf.keras.layers.Dense(20,activation=tf.nn.relu))
+mymodel.add(tf.keras.layers.Dense(20,activation=tf.nn.relu))
+mymodel.add(tf.keras.layers.Dense(10,activation=tf.nn.softmax))
+
+mymodel.compile(optimizer = 'adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+mymodel.fit(x_train,y_train,epochs=3)
+
+pred = mymodel.predict(x_test)
+
+import numpy as np
+
+np.argmax(pred[0])
